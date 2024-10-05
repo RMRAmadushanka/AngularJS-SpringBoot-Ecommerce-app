66dfc427f8fc9c0cbc0f82b666b7407d2105e6c8
RMRAmadushanka <rmramadushanka@gmail.com>
2024-10-05 23:37:14 +0530

    Frontend data fetch

---
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../commom/product';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit{
+  products: Product[] = [];
+  constructor(private productService: ProductService){
+
+  }
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data =>{this.products = data}
+    )
+  }
+
+}
