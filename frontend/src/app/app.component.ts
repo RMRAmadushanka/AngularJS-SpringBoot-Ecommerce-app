d61717c3c10eca272bde5f624b62b058da5680aa
RMRAmadushanka <rmramadushanka@gmail.com>
2024-10-06 11:24:04 +0530

    Update to not standAlone app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ProductListComponent, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
